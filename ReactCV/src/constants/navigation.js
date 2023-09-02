399cd770ea37b0ee449cbaa5bdcb28f83e72bf7d
Ng Zhili <69728128+ngzhili@users.noreply.github.com>
2023-09-03 00:51:30 +0800

    initial commit

---
+++ ReactCV/src/constants/navigation.js
@@ -0,0 +1,20 @@
+const baseUrl = "/AlphaSign";
+
+export const NAV_ITEMS = {
+  WORD_GAME: {
+    to: `${baseUrl}/wordgame`,
+    text: "Word Game",
+  },
+  FOUR_CLASSES: {
+    to: `${baseUrl}/baseline`,
+    text: "Baseline Model",
+  },
+  ALL_CLASSES: {
+    to: `${baseUrl}/extended`,
+    text: "Extended Model",
+  },
+  ABOUT_US: {
+    to: `${baseUrl}/about-us`,
+    text: "About Us",
+  },
+};
